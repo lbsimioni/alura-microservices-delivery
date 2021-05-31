38d0f97c3062306de9ef5de5c0ee5c2d250c2ec9
Leonardo Bucioli Simioni <t1001lnd@pestadorcbmp.com.br>
2021-05-31 18:25:44 -0300

    init project

---
+++ src/main/java/br/com/microservices/furnisher/FurnisherApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.furnisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurnisherApplication.class, args);
+    }
+
+}
